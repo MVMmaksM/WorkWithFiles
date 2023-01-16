6f7d08d40806a3c8bf77cd0348a6ac1cdd8530b7
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-01-16 10:21:14 +0500

    Create FinalTAsk

---
+++ FinalTask/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFile = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\Students.dat";
+            string pathDirectory = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\Students";
+            CreateDirecroty(pathDirectory);
+            WriteStudents(ReadBinary(pathFile), pathDirectory);
+        }
+
+        public static void CreateDirecroty(string pathDirectory)
+        {
+            if (!Directory.Exists(pathDirectory))
+            {
+                Directory.CreateDirectory(pathDirectory);
+            }
+        }
+
+        public static Student[] ReadBinary(string pathFile)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            Student[] students;
+
+            using (var fileStream = new FileStream(pathFile, FileMode.OpenOrCreate))
+            {
+                students = (Student[])binaryFormatter.Deserialize(fileStream);
+            }
+
+            return students;
+        }
+
+        public static void WriteStudents(Student[] students, string pathDirectory) 
+        {          
+            foreach (var student in students)
+            {
+                if (!File.Exists($"{pathDirectory}\\{student.Group}.txt"))
+                {
+                    using (StreamWriter streamWrite = File.CreateText($"{pathDirectory}\\{student.Group}.txt"))
+                    {
+                        streamWrite.WriteLine($"{student.Name} {student.DateOfBirth.Date.ToShortDateString()}");
+                    }
+                }
+                else
+                {
+                    using (StreamWriter streamWrite = new FileInfo($"{pathDirectory}\\{student.Group}.txt").AppendText())
+                    {
+                        streamWrite.WriteLine($"{student.Name} {student.DateOfBirth.ToShortDateString()}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\Students.dat";
+            ReadBinary(path);
+        }
+
+        public static void ReadBinary(string pathFile) 
+        {           
+            
+            BinaryFormatter binaryFormatter = new BinaryFormatter();       
+          
+
+            using (var fileStream = new FileStream(pathFile, FileMode.OpenOrCreate))
+            {
+                var student = (Student[])binaryFormatter.Deserialize(fileStream);
+            }
+        }
+    }
+}
